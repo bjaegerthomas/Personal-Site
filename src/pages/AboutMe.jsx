02cba1a8f854dd19775909a358bfd7fb867a49a4
jaegerthomas <bjaegerthomas@gmail.com>
2025-01-27 10:57:56 -0500

    added resume and aboutme jsx files

---
+++ src/pages/AboutMe.jsx
@@ -0,0 +1,18 @@
+export default function AboutPage() {
+    return (
+        <>
+        <div className="container pt-4">
+                <h2>
+                    About Me
+                </h2>
+                <img src="src/images/headshot.jpg" alt="headshot" />
+        </div>
+        <div>
+            <p>
+                I am a full-stack web developer. I am passionate about creating user-friendly, responsive applications that are both visually appealing and functional. I am proficient in HTML, CSS, JavaScript, Node.js, Express.js, React, and MySQL. I am currently seeking a position as a web developer where I can utilize my skills to create innovative applications that solve real-world problems.
+            </p>
+        </div>
+        </>
+    );
+  }
+  
---
+++ src/pages/Resume.jsx
@@ -0,0 +1,26 @@
+export default function AboutPage() {
+    return (
+      <div className="container pt-4">
+        <h2>
+          Front End Proficiencies
+        </h2>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>Responsive Design</li>
+          <li>React</li>
+          <li>Bootstrap</li>
+        </ul>
+        <h2>
+          Back End Proficiencies
+        </h2>
+        <ul>
+          <li>APIs</li>
+          <li>Node</li>
+          <li>Express</li>
+          <li>MySQL</li>
+        </ul>
+      </div>
+    );
+  }
