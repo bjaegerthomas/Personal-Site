413139e5702d18edf2de741c137076f32aeb2cbc
jaegerthomas <bjaegerthomas@gmail.com>
2025-01-29 14:31:24 -0500

    filled in portfolio page and added render icon

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -9,6 +9,20 @@ export default function Portfolio() {
         <div>
             <img src="src/images/weather.png" alt="weather app" className="weather"/>
         </div>
+        <div>
+            <a href="https://weather-app-xzuf.onrender.com" target="_blank" rel="noopener noreferrer">
+                <img src="src/images/render.png" alt="render" className="icon"/>
+            </a>
+            <a href="https://github.com/bjaegerthomas/Weather-app/tree/main" target="_blank" rel="noopener noreferrer">
+                <img src="src/images/github.png" alt="github" className="icon"/>
+            </a>
+        </div>
+        <div>
+            <p>
+                Click on above icons for a deployed version on Render<br />
+                Or to see the code for the app on Github
+            </p>
+        </div>
         </>
     );
   }
