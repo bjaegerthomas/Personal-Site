83a3f6e1bda7bcbee144eca094118edeec1aeaee
jaegerthomas <bjaegerthomas@gmail.com>
2025-01-28 19:32:38 -0500

    update app to include header and nav

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,10 @@ function App() {
   // The Outlet component will conditionally swap between the different pages according to the URL
   return (
     <>
+      <header className="bg-dark text-light text-left p-3">
+        <h1>Ben Jaeger-Thomas</h1>
       <Nav />
+      </header>
       <main className="mx-3">
         <Outlet />
       </main>
