8be8d004f327dd1d52366143882527e9f0e74085
jaegerthomas <bjaegerthomas@gmail.com>
2025-01-29 14:06:47 -0500

    adding contents and image for portfolio page

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,4 +1,14 @@
-const Portfolio = () => { 
-}
-
-export default Portfolio;
+export default function Portfolio() {
+    return (
+        <>
+        <div className="p-3">
+                <h2 style={{ fontSize: '2.25em', textDecoration: 'underline' }}>
+                    Weather App
+                </h2>
+        </div>
+        <div>
+            <img src="src/images/weather.png" alt="weather app" className="weather"/>
+        </div>
+        </>
+    );
+  }
