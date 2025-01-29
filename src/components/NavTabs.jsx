cd9b95746158ffed664852f014a521ed7159d921
jaegerthomas <bjaegerthomas@gmail.com>
2025-01-28 20:11:13 -0500

    adjusted navtabs to use about or / for home

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -9,7 +9,7 @@ function NavTabs() {
     <ul className="nav nav-tabs">
       <li className="nav-item">
         <Link
-          to="/about"
+          to={currentPage === '/' || currentPage === '/about' ? '/about' : '/'}
           // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage === '/about' ? 'nav-link active' : 'nav-link'}
         >
