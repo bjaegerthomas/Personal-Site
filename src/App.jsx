bf12ed8cb0ec0f9c18dbee465ae9293677ff9bac
jaegerthomas <bjaegerthomas@gmail.com>
2025-01-28 21:24:01 -0500

    added footer to app and added logo icons

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   // The Outlet component will conditionally swap between the different pages according to the URL
   return (
     <>
+    <body>
       <header className="bg-dark text-light text-left p-3">
         <h1>Ben Jaeger-Thomas</h1>
       <Nav />
@@ -13,6 +14,20 @@ function App() {
       <main className="mx-3">
         <Outlet />
       </main>
+      <footer>
+        <div className="footer-icons">
+          <a href="https://github.com/bjaegerthomas" target="_blank" rel="noopener noreferrer">
+            <img src="src/images/github.png" alt="GitHub" className="icon" />
+          </a>
+          <a href="https://www.linkedin.com/in/ben-jaeger-thomas-201071b0/" target="_blank" rel="noopener noreferrer">
+            <img src="src/images/linkedin.png" alt="LinkedIn" className="icon" />
+          </a>
+          <a href="https://www.instagram.com/jaegerthomas/" target="_blank" rel="noopener noreferrer">
+            <img src="src/images/instagram.png" alt="Instagram" className="icon" />
+          </a>
+        </div>
+      </footer>
+      </body>
     </>
   );
 }
