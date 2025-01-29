76a239f4f32f7f0df7de9e07a5f2d2ac37d2124f
jaegerthomas <bjaegerthomas@gmail.com>
2025-01-29 10:43:30 -0500

    updating resume page and overall text alignment for pages

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,33 +1,36 @@
 export default function Resume() {
-    return (
-      <>
-      <div className="container pt-4">
-        <h2>
-          Front End Proficiencies
-        </h2>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JavaScript</li>
-          <li>Responsive Design</li>
-          <li>React</li>
-          <li>Bootstrap</li>
-        </ul>
-        <h2>
-          Back End Proficiencies
-        </h2>
-        <ul>
-          <li>APIs</li>
-          <li>Node</li>
-          <li>Express</li>
-          <li>MySQL</li>
-        </ul>
-      </div>
-      <div>
-        <a href="./images/resume.pdf" download>
+  return (
+    <>
+    <div className="container pt-4">
+      <h2>
+        Front End Proficiencies
+      </h2>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>Responsive Design</li>
+        <li>React</li>
+        <li>Bootstrap</li>
+      </ul>
+      <h2>
+        Back End Proficiencies
+      </h2>
+      <ul>
+        <li>APIs</li>
+        <li>Node</li>
+        <li>Express</li>
+        <li>MySQL</li>
+      </ul>
+      <ul>
+        <li>
+          <a href="./images/resume.pdf" download className="download">
           Download My Resume
-        </a>
-      </div>
-      </>
-    );
-  };
+          </a>
+        </li>
+      </ul>
+    </div>
+    
+    </>
+  );
+};
