9a3ab2041a0715b839fd0f6047df283a84057645
jaegerthomas <bjaegerthomas@gmail.com>
2025-01-29 09:55:04 -0500

    major changes to overall styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ function App() {
   // The Outlet component will conditionally swap between the different pages according to the URL
   return (
     <>
-      <header className="bg-white text-dark blue d-flex justify-content-between align-items-center p-3">
+      <header className="p-3">
         <h1>Ben Jaeger-Thomas</h1>
         <Nav />
       </header>
-      <main className="mx-3" style={{ background: 'linear-gradient(white, teal)' }}>
+      <main className="mx-3">
         <Outlet />
       </main>
       <footer>
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,14 +1,14 @@
 export default function AboutPage() {
     return (
         <>
-        <div className="container pt-4">
-                <h2>
+        <div className="p-3">
+                <h2 style={{ fontSize: '2.25em' }}>
                     About Me
                 </h2>
-                <img src="src/images/headshot.jpg" alt="headshot" />
+                <img src="src/images/headshot.jpg" alt="headshot" className="headshot"/>
         </div>
         <div>
-            <p>
+            <p style={{ fontSize: '1.50em' }}>
                 I am a full-stack web developer. I am passionate about creating user-friendly, responsive applications that are both visually appealing and functional. I am proficient in HTML, CSS, JavaScript, Node.js, Express.js, React, and MySQL. I am currently seeking a position as a web developer where I can utilize my skills to create innovative applications that solve real-world problems.
             </p>
         </div>
