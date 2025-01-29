3859fadd886fbbbe2662efb460d7c4b9772a4250
jaegerthomas <bjaegerthomas@gmail.com>
2025-01-29 18:50:35 -0500

    changed app html format and updated css for new colors

--- src/App.jsx
+++ src/App.jsx
@@ -10,22 +10,24 @@ function App() {
       <h1>Ben Jaeger-Thomas</h1>
       <Nav />
     </header>
-    <main className="mx-3">
-      <Outlet />
-    </main>
-    <footer>
-      <section className="footer-icons">
-        <a href="https://github.com/bjaegerthomas" target="_blank" rel="noopener noreferrer">
-          <img src="src/images/github.png" alt="GitHub" className="icon" />
-        </a>
-        <a href="https://www.linkedin.com/in/ben-jaeger-thomas-201071b0/" target="_blank" rel="noopener noreferrer">
-          <img src="src/images/linkedin.png" alt="LinkedIn" className="icon" />
-        </a>
-        <a href="https://www.instagram.com/jaegerthomas/" target="_blank" rel="noopener noreferrer">
-          <img src="src/images/instagram.png" alt="Instagram" className="icon" />
-        </a>
-      </section>
-    </footer>
+    <body>
+      <main className="mx-3">
+        <Outlet />
+      </main>
+      <footer>
+        <section className="footer-icons">
+          <a href="https://github.com/bjaegerthomas" target="_blank" rel="noopener noreferrer">
+            <img src="src/images/github.png" alt="GitHub" className="icon" />
+          </a>
+          <a href="https://www.linkedin.com/in/ben-jaeger-thomas-201071b0/" target="_blank" rel="noopener noreferrer">
+            <img src="src/images/linkedin.png" alt="LinkedIn" className="icon" />
+          </a>
+          <a href="https://www.instagram.com/jaegerthomas/" target="_blank" rel="noopener noreferrer">
+            <img src="src/images/instagram.png" alt="Instagram" className="icon" />
+          </a>
+        </section>
+      </footer>
+    </body>
     </>
   );
 }
