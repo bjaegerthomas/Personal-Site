6e5d3f3a56a38ea421f4002afce3d40ec2869fa4
jaegerthomas <bjaegerthomas@gmail.com>
2025-01-30 09:24:08 -0500

    adding placeholder html code for portfolio page

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -17,13 +17,135 @@ export default function Portfolio() {
                 <img src="src/images/github.png" alt="github" className="icon"/>
             </a>
         </div>
-        <div></div>
+            <br />
         <div>
             <p>
                 Click on above icons for a deployed version on Render<br />
                 Or to see the code for the app on Github
             </p>
         </div>
+        <br />
+        <div className="p-3">
+                <h2 style={{ fontSize: '2.25em', textDecoration: 'underline' }}>
+                    Coming Soon
+                </h2>
+        </div>
+        <div>
+            <img src="src/images/placeholder.png" alt="place holder" className="weather"/>
+        </div>
+        <div>
+            <a href="https://weather-app-xzuf.onrender.com" target="_blank" rel="noopener noreferrer">
+                <img src="src/images/render.png" alt="render" className="icon"/>
+            </a>
+            <a href="https://github.com/bjaegerthomas/Weather-app/tree/main" target="_blank" rel="noopener noreferrer">
+                <img src="src/images/github.png" alt="github" className="icon"/>
+            </a>
+        </div>
+            <br />
+        <div>
+            <p>
+                Click on above icons for a deployed version on Render<br />
+                Or to see the code for the app on Github
+            </p>
+        </div>
+        <br />
+        <div className="p-3">
+                <h2 style={{ fontSize: '2.25em', textDecoration: 'underline' }}>
+                    Coming Soon
+                </h2>
+        </div>
+        <div>
+            <img src="src/images/placeholder.png" alt="place holder" className="weather"/>
+        </div>
+        <div>
+            <a href="https://weather-app-xzuf.onrender.com" target="_blank" rel="noopener noreferrer">
+                <img src="src/images/render.png" alt="render" className="icon"/>
+            </a>
+            <a href="https://github.com/bjaegerthomas/Weather-app/tree/main" target="_blank" rel="noopener noreferrer">
+                <img src="src/images/github.png" alt="github" className="icon"/>
+            </a>
+        </div>
+            <br />
+        <div>
+            <p>
+                Click on above icons for a deployed version on Render<br />
+                Or to see the code for the app on Github
+            </p>
+        </div>
+        <br />
+        <div className="p-3">
+                <h2 style={{ fontSize: '2.25em', textDecoration: 'underline' }}>
+                    Coming Soon
+                </h2>
+        </div>
+        <div>
+            <img src="src/images/placeholder.png" alt="place holder" className="weather"/>
+        </div>
+        <div>
+            <a href="https://weather-app-xzuf.onrender.com" target="_blank" rel="noopener noreferrer">
+                <img src="src/images/render.png" alt="render" className="icon"/>
+            </a>
+            <a href="https://github.com/bjaegerthomas/Weather-app/tree/main" target="_blank" rel="noopener noreferrer">
+                <img src="src/images/github.png" alt="github" className="icon"/>
+            </a>
+        </div>
+            <br />
+        <div>
+            <p>
+                Click on above icons for a deployed version on Render<br />
+                Or to see the code for the app on Github
+            </p>
+        </div>
+        <br />
+        <div className="p-3">
+                <h2 style={{ fontSize: '2.25em', textDecoration: 'underline' }}>
+                    Coming Soon
+                </h2>
+        </div>
+        <div>
+            <img src="src/images/placeholder.png" alt="place holder" className="weather"/>
+        </div>
+        <div>
+            <a href="https://weather-app-xzuf.onrender.com" target="_blank" rel="noopener noreferrer">
+                <img src="src/images/render.png" alt="render" className="icon"/>
+            </a>
+            <a href="https://github.com/bjaegerthomas/Weather-app/tree/main" target="_blank" rel="noopener noreferrer">
+                <img src="src/images/github.png" alt="github" className="icon"/>
+            </a>
+        </div>
+            <br />
+        <div>
+            <p>
+                Click on above icons for a deployed version on Render<br />
+                Or to see the code for the app on Github
+            </p>
+        </div>
+        <br />
+        <div className="p-3">
+                <h2 style={{ fontSize: '2.25em', textDecoration: 'underline' }}>
+                    Coming Soon
+                </h2>
+        </div>
+        <div>
+            <img src="src/images/placeholder.png" alt="place holder" className="weather"/>
+        </div>
+        <div>
+            <a href="https://weather-app-xzuf.onrender.com" target="_blank" rel="noopener noreferrer">
+                <img src="src/images/render.png" alt="render" className="icon"/>
+            </a>
+            <a href="https://github.com/bjaegerthomas/Weather-app/tree/main" target="_blank" rel="noopener noreferrer">
+                <img src="src/images/github.png" alt="github" className="icon"/>
+            </a>
+        </div>
+            <br />
+        <div>
+            <p>
+                Click on above icons for a deployed version on Render<br />
+                Or to see the code for the app on Github
+            </p>
+        </div>
+        <br />
+        
         </>
     );
   }
