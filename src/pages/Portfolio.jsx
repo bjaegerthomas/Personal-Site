577f9bd3e2330df2a27b23c050134e3e59635cb9
jaegerthomas <bjaegerthomas@gmail.com>
2025-01-30 09:55:58 -0500

    changed padding to accomodateline breaks between apps on portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -9,7 +9,7 @@ export default function Portfolio() {
         <div>
             <img src="src/images/weather.png" alt="weather app" className="weather"/>
         </div>
-        <div>
+        <div id= "port-item">
             <a href="https://weather-app-xzuf.onrender.com" target="_blank" rel="noopener noreferrer">
                 <img src="src/images/render.png" alt="render" className="icon"/>
             </a>
@@ -18,7 +18,31 @@ export default function Portfolio() {
             </a>
         </div>
             <br />
+        <div id= "port-item">
+            <p>
+                Click on above icons for a deployed version on Render<br />
+                Or to see the code for the app on Github
+            </p>
+        </div>
+
+        <div className="p-3">
+                <h2 style={{ fontSize: '2.25em', textDecoration: 'underline' }}>
+                    Coming Soon
+                </h2>
+        </div>
         <div>
+            <img src="src/images/placeholder.png" alt="place holder" className="weather"/>
+        </div>
+        <div id= "port-item">
+            <a href="https://weather-app-xzuf.onrender.com" target="_blank" rel="noopener noreferrer">
+                <img src="src/images/render.png" alt="render" className="icon"/>
+            </a>
+            <a href="https://github.com/bjaegerthomas/Weather-app/tree/main" target="_blank" rel="noopener noreferrer">
+                <img src="src/images/github.png" alt="github" className="icon"/>
+            </a>
+        </div>
+            <br />
+        <div id= "port-item">
             <p>
                 Click on above icons for a deployed version on Render<br />
                 Or to see the code for the app on Github
@@ -33,7 +57,7 @@ export default function Portfolio() {
         <div>
             <img src="src/images/placeholder.png" alt="place holder" className="weather"/>
         </div>
-        <div>
+        <div id= "port-item">
             <a href="https://weather-app-xzuf.onrender.com" target="_blank" rel="noopener noreferrer">
                 <img src="src/images/render.png" alt="render" className="icon"/>
             </a>
@@ -42,31 +66,7 @@ export default function Portfolio() {
             </a>
         </div>
             <br />
-        <div>
-            <p>
-                Click on above icons for a deployed version on Render<br />
-                Or to see the code for the app on Github
-            </p>
-        </div>
-        <br />
-        <div className="p-3">
-                <h2 style={{ fontSize: '2.25em', textDecoration: 'underline' }}>
-                    Coming Soon
-                </h2>
-        </div>
-        <div>
-            <img src="src/images/placeholder.png" alt="place holder" className="weather"/>
-        </div>
-        <div>
-            <a href="https://weather-app-xzuf.onrender.com" target="_blank" rel="noopener noreferrer">
-                <img src="src/images/render.png" alt="render" className="icon"/>
-            </a>
-            <a href="https://github.com/bjaegerthomas/Weather-app/tree/main" target="_blank" rel="noopener noreferrer">
-                <img src="src/images/github.png" alt="github" className="icon"/>
-            </a>
-        </div>
-            <br />
-        <div>
+        <div id= "port-item">
             <p>
                 Click on above icons for a deployed version on Render<br />
                 Or to see the code for the app on Github
@@ -76,11 +76,11 @@ export default function Portfolio() {
                 <h2 style={{ fontSize: '2.25em', textDecoration: 'underline' }}>
                     Coming Soon
                 </h2>
-        </div>
+        </div>  
         <div>
             <img src="src/images/placeholder.png" alt="place holder" className="weather"/>
         </div>
-        <div>
+        <div id= "port-item">
             <a href="https://weather-app-xzuf.onrender.com" target="_blank" rel="noopener noreferrer">
                 <img src="src/images/render.png" alt="render" className="icon"/>
             </a>
@@ -89,7 +89,7 @@ export default function Portfolio() {
             </a>
         </div>
             <br />
-        <div>
+        <div id= "port-item">
             <p>
                 Click on above icons for a deployed version on Render<br />
                 Or to see the code for the app on Github
@@ -103,7 +103,7 @@ export default function Portfolio() {
         <div>
             <img src="src/images/placeholder.png" alt="place holder" className="weather"/>
         </div>
-        <div>
+        <div id= "port-item">
             <a href="https://weather-app-xzuf.onrender.com" target="_blank" rel="noopener noreferrer">
                 <img src="src/images/render.png" alt="render" className="icon"/>
             </a>
@@ -112,7 +112,7 @@ export default function Portfolio() {
             </a>
         </div>
             <br />
-        <div>
+        <div id= "port-item">
             <p>
                 Click on above icons for a deployed version on Render<br />
                 Or to see the code for the app on Github
@@ -126,7 +126,7 @@ export default function Portfolio() {
         <div>
             <img src="src/images/placeholder.png" alt="place holder" className="weather"/>
         </div>
-        <div>
+        <div id= "port-item">
             <a href="https://weather-app-xzuf.onrender.com" target="_blank" rel="noopener noreferrer">
                 <img src="src/images/render.png" alt="render" className="icon"/>
             </a>
@@ -135,7 +135,7 @@ export default function Portfolio() {
             </a>
         </div>
             <br />
-        <div>
+        <div id= "port-item">
             <p>
                 Click on above icons for a deployed version on Render<br />
                 Or to see the code for the app on Github
